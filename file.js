0a4a5d8fd4bd5e7441f73e3a86f1e3b5032fc6a0
alain-cedrick <alaincedrickirakoze@gmail.com>
2024-11-13 11:38:21 +0200

    commit to github

--- file.js
+++ file.js
@@ -1,1 +1,18 @@
 /* DOM Manipulation*/
+const elements = document.getElementsByClassName('intro');
+
+const element = document.querySelector('.intro');
+
+// Create a new element
+const newElement = createElement('p');
+newElement.textContent = 'This is a new paragraph!';
+
+// Append it to an existing element
+const container = getElementById('container');
+container.appendChild(newElement);
+
+const button = cta.getElementsByClassName('.cta');
+
+button.addEventListener('click', function() {
+  alert('Button was clicked!');
+})
